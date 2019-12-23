871f61892bbafb26d808111c6b12765f1744bebb
Nitin Ramrakhiyani <nitin.ramrakhiyani@gmail.com>
2019-12-23 08:30:03 +0530

    Completed the evaluate function.

--- sentence_classification.py
+++ sentence_classification.py
@@ -1,7 +1,10 @@
+import spacy
 import keras
 from keras.models import load_model
 from keras.models import Sequential
 from keras.layers import Dense, Input
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import precision_recall_fscore_support
 
 def train_bert_based_classification(train_data_path, model_save_path, dense_layer_required, network_params):
 	#Reading data
@@ -45,7 +48,7 @@ def train_bert_based_classification(train_data_path, model_save_path, dense_laye
 	model.save(model_save_path)
 
 
-def test_bert_based_classification(test_data_path, model_path, data_preprocessing_required, test_predictions_path):
+def test_bert_based_classification(test_data_path, model_path, test_predictions_path):
 	#Reading data
 	sentences = []
 	test_data = codecs.open(test_data_path, 'r', encoding = 'utf-8', errors = 'ignore')
@@ -81,5 +84,42 @@ def test_bert_based_classification(test_data_path, model_path, data_preprocessin
 	test_predictions_file.close()
 
 
-def evaluate_bert_based_classification(test_data_path, model_path, data_preprocessing_required, test_predictions_path):
+def evaluate_bert_based_classification(test_data_path, model_path, test_predictions_path):
+	#Reading data
+	sentences = []
+	labels = []
+	test_data = codecs.open(test_data_path, 'r', encoding = 'utf-8', errors = 'ignore')
+	for line in test_data:
+		line = line.strip()
+		if(len(line) <= 0):
+			continue
+		sentences.append(line.split('\t')[0])
+		labels.append(line.split('\t')[1])
+	test_data.close()
+
+	#Preparing spacy model loaded with the BERT model
+	nlp = spacy.load("en_trf_bertbaseuncased_lg")
+
+		#Preparing X_test (using spacy_transformer's BERT models)
+	X_test = numpy.zeros((len(sentences), network_params['input_dim']))
+	for i in range(len(sentences)):
+		curr_doc = nlp(sentences[i])
+		X_test[i] = curr_doc._.trf_last_hidden_state[0]
+
+	#Model Loading
+	model = load_model(model_path)
+
+	#Model Predictions
+	predictions = []
+	for i in range(len(sentences)):
+		prediction = model.predict([numpy.array([X_test[i]])])
+		pred = np.argmax(prediction, axis = -1)
+		predictions.append(pred[0])
 	
+	test_predictions_file = codecs.open(test_predictions_path, 'w', encoding = 'utf-8', errors = 'ignore')
+	for k in range(len(predictions)):
+		test_predictions_file.write(sentences[k] + '\t' + str(labels[k]) + '\t' + str(predictions[k]) + '\n')
+	test_predictions_file.close()
+	
+	print (confusion_matrix(labels, predictions)
+	print (precision_recall_fscore_support(labels, predictions))
